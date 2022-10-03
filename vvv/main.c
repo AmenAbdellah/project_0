9afd4ffc60adadc60004851ce9011ceccb06ca61
AmenAbdellah <amenabdellah@gmail.com>
2022-10-04 01:35:00 +0200

    i hope this works

---
+++ vvv/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+ char grade;
+ printf("enter your grade\n");
+ scanf("%c" , &grade);
+
+ switch (grade) {
+
+ case 'A' :
+    printf("waddy neeek");
+    break;
+
+    case 'B' : printf("waddy shwya");
+    break;
+
+    default:
+        printf("enta hattnak 3alena wala ehhh ya khawal");
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+    return 0;
+
+}
+
